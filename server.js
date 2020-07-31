459d24fae3479fe2882a8ff0cd918a058f260f6c
kbrantley <korybrantley@gmail.com>
2020-07-31 15:35:10 -0400

    Fixed delete problem

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const app = express()
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     next();
 });
 
